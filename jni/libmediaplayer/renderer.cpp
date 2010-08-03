bb8d6b6984899d5b4f82ab0059801174ae5a8624
Petr Havlena <havlenapetr@gmail.com>
2010-08-03 18:38:13 +0200

    add methods to renderer

--- jni/libmediaplayer/renderer.cpp
+++ jni/libmediaplayer/renderer.cpp
@@ -1,3 +1,5 @@
+#include <android/log.h>
+
 extern "C" {
 
 #include "libavcodec/avcodec.h"
@@ -5,12 +7,83 @@ extern "C" {
 
 }
 
+#include "output.h"
 #include "renderer.h"
+
+#define TAG "FFMpegRenderer"
 
 Renderer::Renderer()
 {
+	mQueue = new PacketQueue();
 }
 
 Renderer::~Renderer()
 {
+}
+
+bool Renderer::init(JNIEnv *env, jobject jsurface)
+{
+	if(Output::VideoDriver_register(env, jsurface) != ANDROID_SURFACE_RESULT_SUCCESS) {
+		return false;
+	}
+	if(Output::AudioDriver_register() != ANDROID_AUDIOTRACK_RESULT_SUCCESS) {
+		return false;
+	}
+	return true;
+}
+
+PacketQueue* Renderer::queue()
+{
+	return mQueue;
+}
+
+bool Renderer::startAsync(const char* err)
+{
+	pthread_create(&mThread, NULL, startRendering, this);
+    return true;
+}
+
+void* Renderer::startRendering(void* ptr)
+{
+	__android_log_print(ANDROID_LOG_INFO, TAG, "starting renderer thread");
+	Renderer* renderer = (Renderer *) ptr;
+	renderer->mRendering = true;
+    renderer->render(ptr);
+	renderer->mRendering = false;
+	__android_log_print(ANDROID_LOG_INFO, TAG, "decoder renderer ended");
+}
+
+bool Renderer::render(void* ptr)
+{
+	AVPacket        pPacket;
+	
+	while(mRendering)
+    {
+        if(mQueue->get(&pPacket, true) < 0)
+        {
+            mRendering = false;
+            return false;
+        }
+
+        // Free the packet that was allocated by av_read_frame
+        av_free_packet(&pPacket);
+    }
+	
+    return true;
+}
+
+int	Renderer::wait()
+{
+	return pthread_join(mThread, NULL);
+}
+
+void Renderer::stop()
+{
+	mQueue->abort();
+    __android_log_print(ANDROID_LOG_INFO, TAG, "waiting on end of renderer thread");
+    int ret = -1;
+    if((ret = wait()) != 0) {
+        __android_log_print(ANDROID_LOG_ERROR, TAG, "Couldn't cancel renderer: %i", ret);
+        return;
+    }
 }
--- jni/libmediaplayer/renderer.h
+++ jni/libmediaplayer/renderer.h
@@ -1,6 +1,7 @@
 #ifndef FFMPEG_RENDERER_H
 #define FFMPEG_RENDERER_H
 
+#include <jni.h>
 #include <pthread.h>
 
 #include "packetqueue.h"
@@ -10,6 +11,20 @@ class Renderer
 public:
 	Renderer();
 	~Renderer();
+	
+	bool						init(JNIEnv *env, jobject jsurface);
+	PacketQueue*				queue();
+	bool						startAsync(const char* err);
+	int							wait();
+    void						stop();
+	
+private:
+	PacketQueue*                mQueue;
+	pthread_t                   mThread;
+	bool						mRendering;
+	
+	bool						render(void* ptr);
+	static void*				startRendering(void* ptr);
 };
 
 #endif //FFMPEG_DECODER_H
